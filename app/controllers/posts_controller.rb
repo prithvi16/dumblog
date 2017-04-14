092d97c7f0aafe34a82c87867297752b9c7a7cb7
pvsukale <pvsukale@gmail.com>
2017-04-14 15:37:05 +0530

    added dlete edit and update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,22 @@ def show
 end
 
 
+def edit
+    @post = Post.find(params[:id])
+end
+
+def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))    
+end
+
+def destroy
+    @post=Post.find(params[:id])    
+    @post.destroy
+    redirect_to new_post_path
+end
+
 private 
 def post_params
     params.require(:post).permit(:title , :body )
