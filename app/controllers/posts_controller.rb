05c16c3a53f7a668f814bd9fc055292647d5aec5
pvsukale <pvsukale@gmail.com>
2017-04-14 16:41:45 +0530

    added index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,12 @@
 
 class PostsController < ApplicationController
 
+def index
+    @posts=Post.all    
+
+end
+
+
 def new
         @post=Post.new
 
@@ -30,8 +36,10 @@ end
 def destroy
     @post=Post.find(params[:id])    
     @post.destroy
-    redirect_to new_post_path
+    redirect_to posts_path
 end
+
+
 
 private 
 def post_params
