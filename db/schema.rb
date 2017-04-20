4cf74bc97cc169aab2ef8517502a1eba192a865f
pvsukale <pvsukale@gmail.com>
2017-04-20 19:01:50 +0530

    added user to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420120120) do
+ActiveRecord::Schema.define(version: 20170420132946) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20170420120120) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,8 +42,10 @@ ActiveRecord::Schema.define(version: 20170420120120) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "user_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
 end
