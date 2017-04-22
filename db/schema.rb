55e940f9a280c4da76d5113e14e84a532f29bd4d
pvsukale <pvsukale@gmail.com>
2017-04-22 12:15:01 +0530

    added sharing buttons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420132946) do
+ActiveRecord::Schema.define(version: 20170422064347) do
+
+  create_table "data", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_data_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
