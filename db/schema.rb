ecb38f19a810f35e3f39f3f954c3b4e5addeeb9f
pvsukale <pvsukale@gmail.com>
2017-04-22 11:14:58 +0530

    added user links

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170420132946) do
 
-  create_table "comments", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
