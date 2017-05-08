5aa7ce853db29e1c02cf548f7bd662eb2bb17a66
pvsukale <pvsukale@gmail.com>
2017-05-08 12:05:53 +0530

    simple form settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504115449) do
+ActiveRecord::Schema.define(version: 20170508060327) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -111,6 +111,8 @@ ActiveRecord::Schema.define(version: 20170504115449) do
     t.string   "link4t"
     t.string   "link5t"
     t.string   "link6t"
+    t.string   "github"
+    t.string   "twitter"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
